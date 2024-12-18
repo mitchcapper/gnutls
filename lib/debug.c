0fc4a00f91358c68e97b7287735c818e56c81734
Mitch Capper <mitch.capper@gmail.com>
2024-12-17 23:14:35 -0800

    MSVC polyfill and fixes

--- lib/debug.c
+++ lib/debug.c
@@ -28,10 +28,12 @@
 #include "mpi.h"
 
 #ifdef DEBUG
+#define MAX_VECTOR_SIZE 1024*5
+#define DUMP_MPI_BUF_SIZE 400
 void _gnutls_dump_mpi(const char *prefix, bigint_t a)
 {
-	char buf[400];
-	char buf_hex[2 * sizeof(buf) + 1];
+	char buf[DUMP_MPI_BUF_SIZE];
+	char buf_hex[2 * sizeof(char)*DUMP_MPI_BUF_SIZE + 1];
 	size_t n = sizeof buf;
 
 	if (_gnutls_mpi_print(a, buf, &n))
@@ -43,7 +45,7 @@ void _gnutls_dump_mpi(const char *prefix, bigint_t a)
 
 void _gnutls_dump_vector(const char *prefix, const uint8_t *a, size_t a_size)
 {
-	char buf_hex[2 * a_size + 1];
+	char buf_hex[2 * MAX_VECTOR_SIZE + 1];
 
 	_gnutls_debug_log("Vector: length: %d\n\t%s%s\n", (int)a_size, prefix,
 			  _gnutls_bin2hex(a, a_size, buf_hex, sizeof(buf_hex),
